5a20c40264630cdc08e68f442c1aab0493d1c505
SimonGebru <te.gebru@hotmail.se>
2025-04-25 14:50:17 +0200

    delade upp server.js så att anslutning till databas ligger i db.js

--- server.js
+++ server.js
@@ -1,38 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db'); // 👈 Importera nya funktionen
 
-// Ladda miljövariabler från .env-filen
-dotenv.config();
-
-// Importera routes
+// Routes
 const userRoutes = require('./routes/userRoutes');
 const notesRoutes = require('./routes/notesRoutes');
 
-// Skapa appen
+// Init
+dotenv.config();
 const app = express();
+
+// Anslut till databasen
+connectDB(); 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/notes', notesRoutes);
 
-// Anslut till databasen
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Ansluten till databasen');
-  // Starta servern
-  const PORT = process.env.PORT || 5050;
-  app.listen(PORT, () => console.log(`Servern körs på port ${PORT}`));
-})
-.catch((error) => {
-  console.error('Kunde inte ansluta till databasen:', error.message);
-});
+// Starta servern
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(` Servern körs på port ${PORT}`));

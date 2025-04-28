3c13da7ac6ec4871544a64e868772dc5fe5722fb
SimonGebru <te.gebru@hotmail.se>
2025-04-28 13:37:38 +0200

    Klar med PUT och DELETE, ska påbörja sökfunktion och lägga till swagger dokumentation

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -28,3 +28,45 @@ exports.getNotes = async (req, res) => {
     res.status(500).json({ message: 'Kunde inte hämta anteckningar', error: error.message });
   }
 };
+
+// PUT för att uppdatera anteckning
+exports.updateNote = async (req, res) => {
+    const { id } = req.params;
+    const { title, text } = req.body;
+  
+    try {
+      const note = await Note.findOne({ _id: id, userId: req.user.userId });
+  
+      if (!note) {
+        return res.status(404).json({ message: 'Anteckning hittades inte' });
+      }
+  
+      note.title = title || note.title;
+      note.text = text || note.text;
+      note.modifiedAt = new Date();
+  
+      await note.save();
+  
+      res.status(200).json({ message: 'Anteckning uppdaterad', note });
+    } catch (error) {
+      res.status(500).json({ message: 'Kunde inte uppdatera anteckning', error: error.message });
+    }
+  };
+
+
+  // DELETE för att ta bort en anteckning
+  exports.deleteNote = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const note = await Note.findOneAndDelete({ _id: id, userId: req.user.userId });
+  
+      if (!note) {
+        return res.status(404).json({ message: 'Anteckning hittades inte' });
+      }
+  
+      res.status(200).json({ message: 'Anteckning borttagen' });
+    } catch (error) {
+      res.status(500).json({ message: 'Kunde inte ta bort anteckning', error: error.message });
+    }
+  };
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { createNote, getNotes } = require('../controllers/notesController');
+const { createNote, getNotes, updateNote, deleteNote } = require('../controllers/notesController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 
@@ -12,5 +12,11 @@ router.get('/', getNotes);
 
 // POST skapa ny anteckning
 router.post('/', createNote);
+
+// Put ändra en anteckning
+router.put ('/:id', updateNote);
+
+// DELETE för att ta bort en anteckning
+router.delete ('/:id', deleteNote);
 
 module.exports = router;
